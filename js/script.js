23c7515faefa41f717b4531fbfbbfaba2acad4a2
Andre I <aisaac2020@bklawtech.com>
2018-01-23 01:35:52 +0000

    script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*global $*/
 // Pig Latin takes the first consonant (or consonant cluster) of an English 
 // word, moves it to the end of the word and suffixes an ay, or if a word
 // begins with a vowel you just add ay to the end. For example, pig becomes
@@ -8,29 +9,38 @@
 $( document ).ready(function() {
   
   $("#translate").click(function(){
-    var message = $("#input").val();
+     var message = $("#input").val();
     var parrotMessage = pig(message);
     $("#h4").text(parrotMessage);
-});
-
-function pig(word){
- return  word + "ay";    
-}
-
-
-
+  })
+  
 });
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
-
+function wordToPigLatin(word){
+  return  word + "ay";    
+}
 
 
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
+
+function sentenceToPigLatin(sentence){
+  var pigwords = sentence.split(" ") ;
+  
+  for(var i = 0; i < pigwords.length; i++){
+    var word = pigwords[i];
+    var pglatin = wordToPigLatin(word);
+    pigwords[i] = pglatin;
+  }
+  
+  return pigwords;
+};
+
+console.log(sentenceToPigLatin("this is a test"))
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
-
-
+	
