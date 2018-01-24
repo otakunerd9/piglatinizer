20f77f2e67ad910255babcd99ca2e9474c337cdb
Andre I <aisaac2020@bklawtech.com>
2018-01-24 23:29:41 +0000

    script.js

--- js/script.js
+++ js/script.js
@@ -22,12 +22,14 @@ $( document ).ready(function() {
 function wordToPigLatin(word){
   
   if (word[0]==="a" || word[0]==="e" || word[0]==="i" || word[0]==="o" || word[0]==="u" ){
+  
+  return  word + "ay";  
     
   }else{
     
   }
   
-  return  word + "ay";
+  
 }
 
 
